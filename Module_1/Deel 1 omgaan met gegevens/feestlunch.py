f8a5ec8e4f33e4781be1c053f9a76580bbdd6ff1
kyun <99084503@mydavinci.nl>
2025-09-17 10:29:25 +0200

    Update feestlunch.py

--- Module_1/Deel 1 omgaan met gegevens/feestlunch.py
+++ Module_1/Deel 1 omgaan met gegevens/feestlunch.py
@@ -1,5 +1,8 @@
 croissantje = 0.39
 stokbroden = 2.78
 kortingsbonnen = -0.50
-kost = (croissantje * 17) + (stokbroden * 2) + (kortingsbonnen * 3) 
-print (kost)
+aantalcroisant = 17
+aantalstokbrood = 2
+aantalkortingsbonnen = 3
+kost = (croissantje * aantalcroisant) + (stokbroden * aantalstokbrood) + (kortingsbonnen * aantalkortingsbonnen)
+print (f"De feestlunch kost je bij de bakker {kost} euro voor de {aantalcroisant} croissantjes en de {aantalstokbrood} stokbroden als de {aantalkortingsbonnen} kortingsbonnen nog geldig zijn!" )
