0cb19d364a3af333dc6f65c2faab852143f73e42
kyun <99084503@mydavinci.nl>
2025-09-17 10:57:07 +0200

    Update speelhal.py

--- Module_1/Deel 1 omgaan met gegevens/speelhal.py
+++ Module_1/Deel 1 omgaan met gegevens/speelhal.py
@@ -1,6 +1,11 @@
 toegangsticket = 7.45
+mensen = 5
 vr5minuten = 0.37
-minuten5 = 45/5
-kosten = (toegangsticket * 5) + (vr5minuten * minuten5) / 2
+totaalvrminuten = 45
+minuten5 = totaalvrminuten/5
+totaalvrminuten = 45
+vriendendelen = 2
+kosten = (toegangsticket * mensen) + (vr5minuten * minuten5) / vriendendelen
 
-print(kosten)
+
+print(f"Dit geweldige dagje-uit met {mensen} mensen in de Speelhal met {totaalvrminuten} minuten VR kost je maar {kosten} euro per persoon voor {vriendendelen}  mensen")
