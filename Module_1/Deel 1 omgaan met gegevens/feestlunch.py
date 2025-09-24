8bd74e2024ccb93592e62271a572d9350a348c88
kyun <99084503@mydavinci.nl>
2025-09-24 10:29:16 +0200

    Update feestlunch.py

--- Module_1/Deel 1 omgaan met gegevens/feestlunch.py
+++ Module_1/Deel 1 omgaan met gegevens/feestlunch.py
@@ -1,12 +1,14 @@
 
 from termcolor import colored, cprint, COLORS
 
-prijs_croissantje = 0.39
+prijs_croissantje (input = float) ("hoeveel kost een croissant?")
 prijs_stokbroden = 2.78
 kortingsbon = -0.50
-aantalcroisant = 17
-aantalstokbrood = 2
-aantalkortingsbonnen = 3
+
+aantalcroisant = input ("hoeveel croissanten?\n")
+aantalstokbrood = input ("hoeveel stokbroden?\n")
+aantalkortingsbonnen = input ("hoeveel kortingsbonnen\n")
+
 kost = (prijs_croissantje * aantalcroisant) + (prijs_stokbroden * aantalstokbrood) + (kortingsbon * aantalkortingsbonnen)
 
 print (f"De feestlunch kost je bij de bakker {colored(kost, 'green')} euro voor de {colored(aantalcroisant, 'red')} croissantjes en de {colored(aantalstokbrood, 'blue')} stokbroden als de {colored(aantalkortingsbonnen, 'yellow')} kortingsbonnen nog geldig zijn!")
