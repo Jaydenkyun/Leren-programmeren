76de282e1223f209c8cdf729bb342891f8fe1d6d
kyun <99084503@mydavinci.nl>
2025-10-02 13:15:58 +0200

    Create snackuitomaat.py

---
+++ Module_1/Deel 1 omgaan met gegevens/snackuitomaat.py
@@ -0,0 +1,18 @@
+keuze = input("wat wil je uit het automaat halen? kies uit: a,b,c,d of e\n")
+
+a = ("chocoladereep")
+b = ("zakje chips")
+c = ("blikje cola")
+d = ("pakje kauwgom")
+e = ("flesje water")
+
+if keuze == "a":
+    print(f"je krijgt een {a}")
+elif keuze == "b":
+    print(f"je krijgt een {b}")
+elif keuze == "c":
+    print(f"je krijgt een {c}")
+elif keuze == "d":
+    print(f"je krijgt een {d}")
+elif keuze == "e":
+    print(f"je krijgt een {e}")
