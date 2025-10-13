16058df32c61d011c591b81b82147ef123e20ec5
kyun <99084503@mydavinci.nl>
2025-10-13 10:00:07 +0200

    Update order_bugs.py

--- Module_1/deel 3 fouten en uitzonderingen/order_bugs.py
+++ Module_1/deel 3 fouten en uitzonderingen/order_bugs.py
@@ -1,17 +1,16 @@
-else:
-    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
-    resultaat = deel_getallen(getal_1, getal_2)
-if getal_2 == 0:
-    print("Kan niet delen door 0")
-
-getal_2 = vraag_getal("tweede")
-getal_1 = vraag_getal("eerste")
-
 def vraag_getal(aantal):
-    return getal
+    antwoord = input("Voer het " + aantal + " getal in: ")
     getal = int(antwoord)
-    antwoord = input("Voer het "+aantal+" getal in: ")
+    return getal
 
 def deel_getallen(a, b):
-    return antwoord
-    antwoord = a / b
+    return a / b
+
+getal_1 = vraag_getal("eerste")
+getal_2 = vraag_getal("tweede")
+
+if getal_2 == 0:
+    print("Kan niet delen door 0")
+else:
+    resultaat = deel_getallen(getal_1, getal_2)
+    print("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
