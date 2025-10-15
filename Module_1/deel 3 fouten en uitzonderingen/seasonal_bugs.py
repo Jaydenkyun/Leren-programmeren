22a3c32695ebae85da880fda5f0e94e6c760c483
kyun <99084503@mydavinci.nl>
2025-10-15 10:22:15 +0200

    Update seasonal_bugs.py

--- Module_1/deel 3 fouten en uitzonderingen/seasonal_bugs.py
+++ Module_1/deel 3 fouten en uitzonderingen/seasonal_bugs.py
@@ -4,15 +4,16 @@ print('Welk seizoen vind jij het fijnst?',
       "C) Herfst", 
       "D) Winter")
 keuze = input('? ')
+weer_type = ("niets")
 
-if (gekozen_seizoen == 'a' or 'c'):
+if (keuze == 'a' or 'c'):
     print('Dus jij vindt een tussenseizoen het fijnst...')
-elif (gekozen_seizoen != 'a', 'c', 'd'):
+elif (keuze == 'b'):
     weer_type = 'warm'
-else:
+elif(keuze) == 'd':
     weer_type = 'koud'
 
-if len(weer_type) > 0:
+if (weer_type) != 'niets':
     print(f'Dus jij houd van {weer_type} weer!')
-else:
+if keuze not in ('a','b','c','d'):
     print('Dit is geen te kiezen optie!')
