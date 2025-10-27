047f352274d9994a002937f7f4ad9299a2c4ab66
kyun <99084503@mydavinci.nl>
2025-10-27 10:28:42 +0100

    Update ice_bugs.py

--- Module_1/deel 3 fouten en uitzonderingen/ice_bugs.py
+++ Module_1/deel 3 fouten en uitzonderingen/ice_bugs.py
@@ -4,8 +4,8 @@ def convertToEuroText (amount):
 SMALL_PRICE = 1,25
 MEDIUM_PRICE = 2,10
 
-amount = input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE)))
-amount = input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE)))
+amount = input(f'Hoeveel ijsjes van {SMALL_PRICE} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE)))
+amount = input(f'En hoeveel ijsjes van {MEDIUM_PRICE} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE)))
 totalPrice = int(amount * SMALL_PRICE) + int(amount * MEDIUM_PRICE)
 
 print('Dat is dan {} totaal'.format(convertToEuroText(totalPrice)))
