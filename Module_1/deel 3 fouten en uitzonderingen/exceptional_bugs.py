a5d16219b532eee3a37d94192e06bfa471e5ba5b
kyun <99084503@mydavinci.nl>
2025-10-29 12:02:40 +0100

    Update exceptional_bugs.py

--- Module_1/deel 3 fouten en uitzonderingen/exceptional_bugs.py
+++ Module_1/deel 3 fouten en uitzonderingen/exceptional_bugs.py
@@ -1,17 +1,21 @@
 import random
 
 #selecteer 2 nummers
-num1 = str(random.randint(1,10))
-num2 = str(random.randint(5,15))
+num1 = random.randint(1,10)
+num2 = random.randint(5,15)
 
 #vraag om een antwoord
-number = input('Weet jij wat '+num1+'+'+num2+' is? ') 
-
-#geef reactie op het antwoord
 try:
-    if int(number == num1+num2):
+     
+    number = int(input(f'Weet jij wat {num1} + {num2} is? '))
+
+    #geef reactie op het antwoord
+
+    if number == num1 + num2:
         print('Dat is juist')
     else:
         print('Nee dat klopt niet')
-except:
-        print('Dat is geen nummer!')
+
+except ValueError:
+    print('Dat is geen nummer!')
+
