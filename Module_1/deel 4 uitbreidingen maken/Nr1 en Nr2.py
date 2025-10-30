47cbc66ef8adacd64cc18f0a804866f8a61a40b9
kyun <99084503@mydavinci.nl>
2025-10-30 12:14:50 +0100

    min of max toegevoegd en extra een error voor als dat nodig is

--- Module_1/deel 4 uitbreidingen maken/Nr1 en Nr2.py
+++
@@ -1,9 +0,0 @@
-nr1 = (input("Geef het eerste gehele getal (nr1): "))
-nr2 = (input("Geef het tweede gehele getal (nr2): "))
-
-if nr1 == nr2:
-    print(f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})')
-elif nr1 > nr2:
-    print(f'nr1 ({nr1}) is groter dan nr2 ({nr2})')
-else:
-    print(f'nr2 ({nr2}) is groter dan nr1 ({nr1})')
---
+++ Module_1/deel 4 uitbreidingen maken/min en max.py
@@ -0,0 +1,20 @@
+nr1 = float(input("Geef het eerste gehele getal (nr1): "))
+nr2 = float(input("Geef het tweede gehele getal (nr2): "))
+
+minimum = nr1
+maximum = nr2
+verschil = maximum - minimum
+try:
+    if nr1 == nr2:
+        print(f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})')
+    elif nr1 > nr2:
+        print(f'nr1 ({nr1}) is groter dan nr2 ({nr2})')
+        minimum = nr2
+        maximum = nr1
+    else:
+        print(f'nr2 ({nr2}) is groter dan nr1 ({nr1})')
+        minimum = nr1
+        maximum = nr2
+    print(f"Het minimum is: {minimum} en het maximimum is: {maximum}")
+except ValueError:
+    print("dit kan niet")
