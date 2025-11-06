5c03c7e1192192d1228a58f12386964e8e51ad2d
kyun <99084503@mydavinci.nl>
2025-11-06 09:46:39 +0100

    Update dungeon.py

--- Module_1/proef dungeonkeeper/dungeon.py
+++ Module_1/proef dungeonkeeper/dungeon.py
@@ -4,14 +4,6 @@ player_attack = 1
 player_defense = 0
 player_health = 3
 sleutel = False
-
-
-
-som_getal_1 =random.randint(10,25)
-som_getal_2 =random.randint(-5,75)
-operator_list = ["+","-","*"]
-operator = random.choice(operator_list)
-
 # === [kamer 1] === #
 print('Door de twee grote deuren loop je een gang binnen.')
 print('Het ruikt hier muf en vochtig.')
@@ -20,11 +12,17 @@ print('')
 time.sleep(1)
 
 # === [kamer 2] === #
-print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
-print('Het standbeeld heeft een sleutel vast.')
-print('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
-print(f"Daarboven zie je een som staan {som_getal_1} {operator} {som_getal_2} =")
-antwoord = int(input('Wat toets je in?\n'))
+
+som_getal_1 = random.randint(10, 25)
+som_getal_2 = random.randint(-5, 75)
+operator_list = ["+", "-", "*"]
+operator = random.choice(operator_list)
+
+print ('Je stapt door de deur heen en je ziet een standbeeld voor je.')
+print ('Het standbeeld heeft een sleutel vast.')
+print ('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
+print (f'Daarboven zie je een som staan {som_getal_1}{operator}{som_getal_2}=?')
+antwoord = int(input('Wat toets je in?'))
 
 if antwoord == eval(f"{som_getal_1}{operator}{som_getal_2}"):
     print('Het stadbeeld laat de sleutel vallen en je pakt het op')
@@ -32,94 +30,91 @@ if antwoord == eval(f"{som_getal_1}{operator}{som_getal_2}"):
 else:
     print('Er gebeurt niets....')
 
-print('Je zie een deur achter het standbeeld.')
+print('Je zie een 2 deuren achter het standbeeld.')
+print('deur 1 lijd naar kaamer 6 en deur 2 lijd naar kaamer 3')
+deur_keuze = input('welke deur kies je?')
 print('')
 time.sleep(1)
 
-# === [kamer 6] === #
-zombie_attack = 1
-zombie_defense = 0
-zombie_health = 2
-print(f'Dapper loop je de kamer binnen.')
-print('Je loopt tegen een zombie aan.')
+if deur_keuze == 1:
+   # === [kamer 6] === #
+   zombie_attack = 1
+   zombie_defense = 0
+   zombie_health = 2
+   print('Je duwt hem open en stap een donkere kamer binnen.')
+   print('Je loopt tegen een zombie aan.')
 
-zombie_hit_damage = (zombie_attack - player_defense)
-if zombie_hit_damage <= 0:
-    print('Jij hebt een te goede verdediging voor de zombie, hij kan je geen schade doen.')
-else:
+   zombie_hit_damage = (zombie_attack - player_defense)
+   if zombie_hit_damage <= 0:
+    print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
+   else:
     zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
     
     player_hit_damage = (player_attack - zombie_defense)
     player_attack_amount = math.ceil(zombie_health / player_hit_damage)
 
     if player_attack_amount < zombie_attack_amount:
-        player_health -= zombie_attack * zombie_attack_amount
+        player_health -= zombie_attack * player_attack_amount
         print(f'In {player_attack_amount} rondes versla je de zombie.')
         print(f'Je health is nu {player_health}.')
     else:
         print('Helaas is de zombie te sterk voor je.')
         print('Game over.')
-        exit()
-
-zombie_attack = 1
-zombie_defense = 0
-zombie_health = 2
-print(f'Dapper loop je de kamer binnen.')
-print('Je loopt tegen een zombie aan.')
-
+print('')
+time.sleep(1)
 
 
 
 
 # === [kamer 3] === #
-lootpool_kamer3 = ["schild","zwaard"]
-item = random.choice(lootpool_kamer3)
-if item == ("schild"):
+lootpool_room3 = ["schild", "zwaard"]
+item = random.choice(lootpool_room3)
+if item == "schild":
     player_defense += 1
 else:
-    player_attack +=2
+    player_attack += 2
 
-print('Je duwt hem open en stap een hele lange kamer binnen.')
+print('Je stapt een hele lange kamer binnen.')
 print(f'In deze kamer staat een tafel met daarop een {item}.')
 print(f'Je pakt het {item} op en houd het bij je.')
 print('Op naar de volgende deur.')
 print('')
 time.sleep(1)
 
-
 # === [kamer 4] === #
-BOSS_zombie_attack = 2
-BOSS_zombie_defense = 0
-BOSS_zombie_health = 3
-print(f'Dapper loop je de kamer binnen met jouw nieuwe {item}.')
-print('Je loopt tegen een zombie aan.')
+boss_attack = 1
+boss_defense = 0
+boss_health = 2
+print(f'Dapper loop je de kamer binnen met je nieuwe {item}.')
+print('Je ziet de dungeon boss staan.')
 
-BOSS_zombie_hit_damage = (BOSS_zombie_attack - player_defense)
-if BOSS_zombie_hit_damage <= 0:
-    print('Jij hebt een te goede verdediging voor de zombie, hij kan je geen schade doen.')
+boss_hit_damage = (boss_attack - player_defense)
+if boss_hit_damage <= 0:
+    print('Jij hebt een te goede verdedigign voor de boss, hij kan je geen schade doen.')
 else:
-    BOSS_zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
+    boss_attack_amount = math.ceil(player_health / boss_hit_damage)
     
-    player_hit_damage = (player_attack - BOSS_zombie_defense)
-    player_attack_amount = math.ceil(BOSS_zombie_health / player_hit_damage)
+    player_hit_damage = (player_attack - boss_defense)
+    player_attack_amount = math.ceil(boss_health / player_hit_damage)
 
-    if player_attack_amount < BOSS_zombie_attack_amount:
-        player_health -= zombie_attack * zombie_attack_amount
-        print(f'In {player_attack_amount} rondes versla je de zombie.')
+    if player_attack_amount < boss_attack_amount:
+        player_health -= boss_attack * player_attack_amount
+        print(f'In {player_attack_amount} rondes versla je de boss.')
         print(f'Je health is nu {player_health}.')
     else:
-        print('Helaas is de zombie te sterk voor je.')
+        print('Helaas is de boss te sterk voor je.')
         print('Game over.')
         exit()
+print('')
+time.sleep(1)
+
 
 # === [kamer 5] === #
 print('Voorzichtig open je de deur, je wilt niet nog een zombie tegenkomen.')
 print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
 print('Je loopt er naartoe.')
 
-
-
 if sleutel == True:
-    print("je opent de kist en hebt gewonnen!!!")
+    print ("jippie je hebt gewonnen")
 else:
-    print("je kan de kist niet openen dus je hebt verloren")
+    print ("ga terug om die sleutel te haalen raar aapje")
