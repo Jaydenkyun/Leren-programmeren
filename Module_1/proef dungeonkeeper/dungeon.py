568d378c5408ef67a49c884284cf5101546e8e49
kyun <99084503@mydavinci.nl>
2025-11-06 12:18:16 +0100

    Update dungeon.py

--- Module_1/proef dungeonkeeper/dungeon.py
+++ Module_1/proef dungeonkeeper/dungeon.py
@@ -85,7 +85,11 @@ if deur_keuze1 == "1":
     print(f'In deze kamer staat een tafel met daarop een bord waarop staat zwaard en schild te koop.')
     print(f'Achter het bord staat een goblin die aan jouw vraagt of jij iets zou willen kopen.')
     print("je krijgt de kans om wat te komen van jouw ene rupee en toevallig kost het zwaard een rupee en het schild een rupee dus je krijgt de keus: koop jij het zwaard? koop jij het schild? of koop je helemaal niets en loop je door?")
-    shop_items_keuze = input("typ: (schild) of (zwaard) of (geen) als je geen wapen wilt kopen ")
+    if player_rupee_amount == 1:
+        shop_items_keuze = input("typ: (schild) of (zwaard) of (geen) als je geen wapen wilt kopen")
+    else:
+        shop_items_keuze = input("typ: (schild) of (zwaard) of (bijde) of (geen) als je geen wapen wilt kopen")
+    
     print('Op naar de volgende deur.')
     print('')
     time.sleep(1)
@@ -96,10 +100,18 @@ if deur_keuze1 == "1":
     elif shop_items_keuze == "zwaard":
         player_rupee_amount -= 1
         player_attack += 2
-    else:
+    elif shop_items_keuze == "beide":
+        player_rupee_amount -= 2
+        player_defense += 1
+        player_attack_amount += 2
+    elif shop_items_keuze == "geen":
         player_attack += 0
         print("je bent nu officieël een jood")
 else:
+    # === [kamer 8] === #
+
+    # === [kamer 3] === #
+
     print('Je stapt een hele lange kamer binnen.')
     print(f'In deze kamer staat een tafel met daarop een bord waarop staat zwaard en schild te koop.')
     print(f'Achter het bord staat een goblin die aan jouw vraagt of jij iets zou willen kopen.')
@@ -108,6 +120,7 @@ else:
     print('Op naar de volgende deur.')
     print('')
     time.sleep(1)
+
 
     if shop_items_keuze == "schild":
         player_rupee_amount -= 1
