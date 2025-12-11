fe376ee013a53885f4d9a318aa06421d3d744af9
kyun <99084503@mydavinci.nl>
2025-12-11 15:46:27 +0100

    Update coins.py

--- Module_2/deel_1/coins.py
+++ Module_2/deel_1/coins.py
@@ -1,28 +1,34 @@
-# name of student: 
-# number of student:
-# purpose of program: 
-# structure of program: 
-coinValues = [500, 200, 100, 50, 20, 10, 5, 2, 1]  # lijst van muntwaardes in centen)
+coinValues = [500, 200, 100, 50, 20, 10, 5, 2, 1]  #lijst met de waarde van de muntne
 
-toPay = int(float(input('Amount to pay: ')) * 100)  # veranderen naar centen
-paid = int(float(input('Paid amount: ')) * 100)     # hetzelfde voor het betaalde bedrag, ook in centen
-change = paid - toPay                               # bereken hoeveel wisselgeld terug moet worden gegeven
+toPay = int(float(input('Amount to pay: ')) * 100) #de bedragen invullen en omzetten naar centen
+paid = int(float(input('Paid amount: ')) * 100)  #de bedragen invullen en omzetten naar centen
+change = paid - toPay
 
-returnedCoins = {}  # dictionary voor de verschillende muntensoorten
 
-while change > 0 and len(coinValues) > 0:  # while loopt zolang er wisselgeld nodig is en er nog muntsoorten in de lijst zitten
+returnedCoins = {} # Dictionary voor overzicht van teruggegeven munten, want die worden hier dan zo toegevoegd
 
-    coinValue = coinValues.pop(0)         # pop(0) verwijdert en retourneert het eerste element uit de lijst (grootste munt eerst)
-    nrCoins = change // coinValue         # // is floor division; geeft hoeveel van deze munt in het resterende change past
 
-    if nrCoins > 0:  # alleen relevante munten tonen (waar er minimaal 1 van teruggegeven kán worden)
+#While-loop om wisselgeld stap voor stap terug te geven
+while change > 0 and len(coinValues) > 0: #len geeft aan hoeveel verschillende dingen in de lijst staan
+    coinValue = coinValues.pop(0) #pop verwijderd het eerste ding uit de lijst zodat hij niet nog een keer langs hoeft te komen
+    nrCoins = change // coinValue
 
-        print('Return maximal', nrCoins, 'coins of', coinValue, 'cents!')  
-        
-        nrCoinsReturned = int(input('How many coins of ' 
-                                    + str(coinValue) +
-                                    ' cents did you return? '))  # gebruiker vult echt aantal teruggegeven munten in
+    if nrCoins > 0:
+        print('Return maximal', nrCoins, 'coins of', coinValue, 'cents!') # printje om voor de gebruiker duidelijk te maken hoeveel jij van deze euro of centen terug kan geven
+        nrCoinsReturned = int(input('How many coins of ' + str(coinValue) + ' cents did you return? ')) # input voor hoeveel geld jij van deze soort heb je
 
-        change -= nrCoinsReturned * coinValue   # aftrekken van het resterende wisselgeld
+        # Opslaan in overzicht
+        returnedCoins[coinValue] = nrCoinsReturned
 
-        returnedCoins[coinValue] = nrCoinsReturned  # opslaan in dictionary voor later overzicht
+        change -= nrCoinsReturned * coinValue # wisselgeld minder maken
+
+print("\n--- teruggeven van de munten overzicht ---")
+for coin, amount in returnedCoins.items():        # Overzicht printen na de loop
+    print(f"{coin} cent: {amount} munt(en) teruggegeven")
+
+# Melding als het wisselgeld niet helemaal of goed is teruggegeven
+if change > 0: 
+    print("\n Niet al het wisselgeld is teruggegeven")
+    print("Nog te geven:", change, "cent")
+else:
+    print("\nAlles is goed teruggegeven")
