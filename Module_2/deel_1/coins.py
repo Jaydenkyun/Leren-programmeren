587c54a53cb88d06e827cb41b10219e9e371fca7
kyun <99084503@mydavinci.nl>
2025-12-11 11:40:58 +0100

    verder

--- Module_2/deel_1/coins.py
+++ Module_2/deel_1/coins.py
@@ -2,8 +2,7 @@
 # number of student:
 # purpose of program: 
 # structure of program: 
-
-coinValues = [50,20,10,5,2,1] #
+coinValues = [500,200,100,50,20,10,5,2,1] #
 
 toPay = int(float(input('Amount to pay: '))* 100) #
 
@@ -20,6 +19,7 @@ while change > 0 and len(coinValues) > 0: #
   nrCoins = change // coinValue #
 
   if nrCoins > 0: #
+     
     print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #
     nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #
     change -= nrCoinsReturned * coinValue #
