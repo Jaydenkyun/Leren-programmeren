f6a95febe43807d3a8c7a7404d1aead4ab628bb2
kyun <99084503@mydavinci.nl>
2026-01-22 13:19:49 +0100

    Update the_range_factory.py

--- Module_2/deel_2/the_range_factory.py
+++ Module_2/deel_2/the_range_factory.py
@@ -2,14 +2,14 @@ aantal = int(input("Hoeveel lijstjes wil je? "))
 
 alles = []
 tekst = ""
-begin = 1
+begin = 1 # hij start bij een
 
-for i in range(aantal):
-    invoer = int(input(f"Geef waarden voor lijstje {i+1} "))
+for i in range(aantal): #voor elk lijstje dat jij wilt doet hij dit opnieuw in een loop
+    invoer = int(input(f"Geef waarden voor lijstje {i+1} ")) #hier vraagt hij welke waarden jij het lijstje wil geven +1 is er om niet bij lijstje 0 te kunnen beginnen
     for j in (range(invoer)):
         tekst += str(begin+j) + ","
 
     alles.append(tekst)
     tekst = ""
-    begin += invoer
+    begin += invoer #hier onthoud hij waar het ene lijstje is beïndigd om het volgende lijtje te beginnen
 print(alles)
