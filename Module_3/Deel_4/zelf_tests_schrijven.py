cc3ae42f596b319044ad75a04331e0bd01b31a69
kyun <99084503@mydavinci.nl>
2026-05-21 15:08:54 +0200

    naam_leeftijd output opdracht aangemaakt

--- Module_3/Deel_4/zelf_tests_schrijven.py
+++ Module_3/Deel_4/zelf_tests_schrijven.py
@@ -1,8 +1,5 @@
 from test_lib import test, report
 
-test('grootste 1', 10, grootste(10, 5))
-test('grootste 2', 7, grootste(3, 7))
-test('grootste gelijk', 4, grootste(4, 4))
-test('grootste negatief', -2, grootste(-2, -5))
+
 
 report()
