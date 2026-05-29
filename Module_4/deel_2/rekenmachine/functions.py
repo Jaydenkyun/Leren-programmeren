53b62567f5c8e20c5d1ba838d98209952d0c57f5
kyun <99084503@mydavinci.nl>
2026-05-29 11:55:40 +0200

    functions aangemaakt voor de rekenmachine en begonnen met de rekenmachine

---
+++ Module_4/deel_2/rekenmachine/functions.py
@@ -0,0 +1,11 @@
+def addition(n1, n2):
+    return float(n1) + float(n2)
+
+def subtraction(n1, n2):
+    return float(n1) - float(n2)
+
+def multiplication(n1, n2):
+    return float(n1) * float(n2)
+
+def devision(n1, n2):
+    return float(n1) / float(n2)
---
+++ Module_4/deel_2/rekenmachine/rekenmachine.py
@@ -0,0 +1,35 @@
+import functions 
+
+print("wat wilt u doen?")
+print("A) getallen optellen")
+print("B)  getallen aftrekken")
+print("C) getallen vermenigvuldigen")
+print("D) getallen delen")
+print("E) getallen ophogen")
+print("F) getallen verlagen")
+print("G) getallen verdubbelen")
+print("H) getallen halveren")
+print("I) niets")
+functie = input("kies:")
+if functie == "I":
+    exit()
+elif functie == "A":
+    print(f"{functie} getallen optellen")
+    n1 = input("welk getal?")
+    n2 = input(f"welk getal wil je optellen bij {n1}\n")
+    print(functions.addition(n1, n2))
+   
+elif functie == "B":
+    print("welk getal?")
+    
+#elif functie == "C":
+    
+#elif functie == "D":
+    
+#elif functie == "E":
+    
+#elif functie == "F":
+    
+#elif functie == "G":
+    
+#elif functie == "H":
