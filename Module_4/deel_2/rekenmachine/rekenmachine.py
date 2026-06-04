3c22ff6114dc845e09a7ec5d725ed0e1822727cd
kyun <99084503@mydavinci.nl>
2026-06-04 15:00:37 +0200

    rekenmachine aangepast

--- Module_4/deel_2/rekenmachine/rekenmachine.py
+++ Module_4/deel_2/rekenmachine/rekenmachine.py
@@ -1,67 +1,70 @@
 import functions 
-
-print("wat wilt u doen?")
-print("A) getallen optellen")
-print("B)  getallen aftrekken")
-print("C) getallen vermenigvuldigen")
-print("D) getallen delen")
-print("E) getallen ophogen")
-print("F) getallen verlagen")
-print("G) getallen verdubbelen")
-print("H) getallen halveren")
-print("I) niets")
-functie = input("kies:").upper()
-
-
-
 while True:
-    try:
-        n1 = float(input("welk getal?"))
-        break
-    except ValueError: 
-        print("voer een getal in")
-while True:
-    try:   
-        n2 = float(input(f"welk getal wil je optellen bij {n1}\n"))
-        break
-    except ValueError:
-        print("voer een getal in")
-        
-if functie == "I" or "":
-    print("einde berekening")
-    exit()
+    print("wat wilt u doen?")
+    print("A) optellen")
+    print("B) aftrekken")
+    print("C) vermenigvuldigen")
+    print("D) delen")
+    print("E) ophogen")
+    print("F) verlagen")
+    print("G) verdubbelen")
+    print("H) halveren")
+    print("I) niets")
+    functie = input("kies:").upper()
 
-elif functie == "A":
-    print(f"{functie} getallen optellen")
-    print(functions.addition(n1, n2))
-   
-elif functie == "B":
-    print(f"{functie} getallen aftrekken")
-    print(functions.subtraction(n1, n2))
+    if functie == "I" or functie == "":
+        print("einde berekening")
+        exit()
 
-elif functie == "C":
-    print(f"{functie} getallen vermenigvuldigen")
-    print(functions.multiplication(n1, n2))
 
-elif functie == "D":
-    print(f"{functie} getallen delen")
-    print(functions.devision(n1, n2))
+    while True:
+        try:
+            n1 = float(input("welk getal?"))
+            break
+        except ValueError: 
+            print("voer getal 1 in in")
+    while True:
+        try:   
+            n2 = float(input("voer getal 2 in"))
+            break
+        except ValueError:
+            print("voer een getal in")
 
-elif functie == "E":
-    print(f"{functie} getal ophogen")
-    if n2 == "":
-        n2 = "1"
-    print(functions.addition(n1, n2))
-
-elif functie == "F":
-    print(f"{functie} getal verlagen")
-    if n2 == "":
-        n2 = "1"
-    print(functions.subtraction(n1, n2))
-
-elif functie == "G":
-    n2 = 2
-    print(functions.devision(n1, n2))
+    if functie == "A":
+        print(f"{functie} getallen optellen")
+        print(functions.addition(n1, n2))
     
-elif functie == "H":
-    print(functions.devision(n1, n2))
+    elif functie == "B":
+        print(f"{functie} getallen aftrekken")
+        print(functions.subtraction(n1, n2))
+
+    elif functie == "C":
+        print(f"{functie} getallen vermenigvuldigen")
+        print(functions.multiplication(n1, n2))
+
+    elif functie == "D":
+        print(f"{functie} getallen delen")
+        print(functions.devision(n1, n2))
+
+    elif functie == "E":
+        print(f"{functie} getal ophogen")
+        if n2 == "":
+            n2 = "1"
+        print(functions.addition(n1, n2))
+
+    elif functie == "F":
+        print(f"{functie} getal verlagen")
+        if n2 == "":
+            n2 = "1"
+        print(functions.subtraction(n1, n2))
+
+    elif functie == "G":
+        n2 = 2
+        print(functions.devision(n1, n2))
+        
+    elif functie == "H":
+        print(functions.devision(n1, n2))
+
+    doorgaan = input("wil je nog meer berekeningen doen? typ: ja of nee")
+    if doorgaan == ("nee").upper():
+        break
